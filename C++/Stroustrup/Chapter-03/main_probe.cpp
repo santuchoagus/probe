19326df6460ff940d60b1b78c68863d5ce6fc187
santuchoagus <97124374+santuchoagus@users.noreply.github.com>
2024-09-20 02:44:20 -0300

    Chapter-03 probe notes

--- C++/Stroustrup/Chapter-03/main_probe.cpp
+++ C++/Stroustrup/Chapter-03/main_probe.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
-	std::cout << "Hello, world\n";
-	return 0;
+using namespace std;
+
+int main() {
+    string name;
+    int number;
+
+    // This will get a random number from memory on each execution
+    int initnumber;
+    cout << initnumber << endl;
+
+    std::cout << "Insert a name please: ";
+    std::cin >> name;
+    std::cout << "Welcome " << name << "!\n";
+
+    std::cout << "Insert a number please: ";
+    std::cin >> number;
+    std::cout << "Your number is " << number << "!\n";
+
+    // Version 2, two consecutive cin
+    std::cout << "Insert a name followed by a number please: ";
+    std::cin >> name;
+    std::cin >> number;
+    std::cout << "Welcome " << name << " Your number is " << number << "!\n";
+    return 0;
 }
+
---
+++ C++/Stroustrup/Chapter-03/quicktest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // This will get a random number from memory on each execution
+    int initnumber;
+    cout << initnumber;
+    return 0;
+}
